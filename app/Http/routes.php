f071b8fc8dab0804c1cb252f0f8409277f2d67ec
fengshiyou <501453944@qq.com>
2017-06-22 16:00:06 +0800

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/bluesecretaryHook','GithubHookController@index');
+
 Route::get('/z', function () {
     return 3;
 });
