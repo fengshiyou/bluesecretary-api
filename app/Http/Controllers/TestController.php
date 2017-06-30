6b2980b56242895b4064340aaa918a9c9d45fbb1
fengshiyou <501453944@qq.com>
2017-06-30 16:26:44 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,44 +12,11 @@ use Illuminate\Support\Facades\Queue;
 
 class TestController extends Controller
 {
-    /**
-     * @api {get} /test 接口测试
-     * @apiDescription 根据ID（id）获取列表信息
-     * @apiGroup test APIs
-     *
-     *
-     * @apiParamExample {string} 请求参数格式:
-     *    ?id=123&page=1&perpage=20
-     *
-     * @apiVersion 1.0.0
-     * @apiErrorExample {json} 错误返回值:
-     *     {
-     *        "code": 10003,
-     *        "msg": "ParametersError [Method]:get_tests参数错误!",
-     *        "error": {
-     *            "id": "",
-     *            "page": "",
-     *            "perpage": ""
-     *        },
-     *       "status": "fail"
-     *     }
-     * @apiSuccessExample {json} 正确返回值:
-     *     {
-     *   "code": 0,
-     *   "msg": "OK ",
-     *   "data": [
-     *       {
-     *           "id": "622051004185471233",
-     *           "testCode": "000050",
-     *       }
-     *   ],
-     *   "status": "ok",
-     *   "count": "14"
-     *   }
-     */
+
     public function index()
     {
         //
+        return resp_suc();
         Event::setQueueResolver(function () {
             return Queue::connection('captcha');
         })->fire(new TestEvent(55,66));
